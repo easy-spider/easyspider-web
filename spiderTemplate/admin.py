fbdbdef3c38d90510f47a2bdb6f02cada40121d5
ZZy979 <979481894@qq.com>
2020-05-02 01:58:33 +0800

    Finish views for app spiderTemplate (except creating task)

--- spiderTemplate/admin.py
+++ spiderTemplate/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 from spiderTemplate import models
 
+admin.site.register(models.SiteType)
 admin.site.register(models.Site)
 admin.site.register(models.Template)
 admin.site.register(models.Field)
-admin.site.register(models.InputType)
 admin.site.register(models.Param)
