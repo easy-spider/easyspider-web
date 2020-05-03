83768ca19306f604801c1a82a5265aba558c0b0d
Ssynchronicity <1206736488@qq.com>
2020-05-03 11:28:27 +0800

    增加channels相关代码

---
+++ task/consumers.py
@@ -0,0 +1,68 @@
+# chat/consumers.py
+from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+import json
+
+
+class TaskConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user_pk = self.scope['url_route']['kwargs']['user_pk']
+        self.task_pk = self.scope['url_route']['kwargs']['task_pk']
+        self.group_name = 'task_%s_%s' % (self.user_pk, self.task_pk)
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'task_message',
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    async def task_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+
+def send_group_msg(user_pk, task_pk, message):
+    # 从Channels的外部发送消息给Channel
+    """
+    :param user_pk:
+    :param task_pk:
+    :param message:
+    :return:
+    """
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        'task_{}_{}'.format(user_pk, task_pk),  # 构造Channels组名称
+        {
+            "type": "task_message",
+            "message": message,
+        }
+    )
