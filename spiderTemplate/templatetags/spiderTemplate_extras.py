0cf7621300d2139a92aeb76a0b14754b8277ee51
Ssynchronicity <1206736488@qq.com>
2020-05-12 11:42:44 +0800

    完成task app下html的修改

--- spiderTemplate/templatetags/spiderTemplate_extras.py
+++ spiderTemplate/templatetags/spiderTemplate_extras.py
@@ -7,3 +7,9 @@ CARD_EACH_ROW = 4
 @register.filter
 def remainder_list(value):
     return range(0, CARD_EACH_ROW - value % CARD_EACH_ROW)
+
+
+@register.filter
+def get_dict_value(my_dict, key):
+    """获取dict中的值"""
+    return my_dict[key]
